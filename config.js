36c769d5d024c09bf73f3ba01be21b85e817be4d
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-08-22 16:34:22 -0400

    Update Site

--- config.js
+++ config.js
@@ -143,7 +143,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1755894859085_0",
+      "id": "q_auto_1755894861387_0",
       "questionText": "How long have you lived in your current home?",
       "options": [
         {
@@ -165,7 +165,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1755894859085_1",
+      "id": "q_auto_1755894861387_1",
       "questionText": "Are you planning to buy or sell property in the next 12 months?",
       "options": [
         {
@@ -187,7 +187,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1755894859085_2",
+      "id": "q_auto_1755894861387_2",
       "questionText": "What do you value most in your home dining experiences?",
       "options": [
         {
@@ -209,7 +209,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1755894859085_3",
+      "id": "q_auto_1755894861387_3",
       "questionText": "Would you like tips on how to host great dinners or info about Toronto real estate trends?",
       "options": [
         {
