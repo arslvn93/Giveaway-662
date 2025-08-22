e23b2967b82500f8ca3155846f2114c66e49e4c6
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-08-22 17:07:38 -0400

    Update Site

--- config.js
+++ config.js
@@ -147,7 +147,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1755896563624_0",
+      "id": "q_auto_1755896857764_0",
       "questionText": "What do you like the most about your current home?",
       "options": [
         {
@@ -169,7 +169,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1755896563624_1",
+      "id": "q_auto_1755896857764_1",
       "questionText": "Is a move on your radar in the near future?",
       "options": [
         {
@@ -191,7 +191,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1755896563624_2",
+      "id": "q_auto_1755896857764_2",
       "questionText": "What’s your current housing situation?",
       "options": [
         {
@@ -237,8 +237,8 @@ const config = {
       "email": "Daniel@ErbaAndco.com"
     },
     "footerDisclaimer": "This giveaway is organized by Daniel Losier and is not sponsored by any chef service. Winner chosen at random from eligible entries received by Sept 7, 2025. Must be 18+ and live in Toronto area.",
-    "organizerLogoUrl": "file:///Users/mateojackson/Downloads/LOSIER_PrimaryLogo_Pistachio%20(1).webp",
-    "secondaryLogoUrl": "file:///Users/mateojackson/Downloads/RealBrokerLogo.webp",
+    "organizerLogoUrl": "https://erbaandco.com/wp-content/uploads/2023/03/LOSIER_PrimaryLogo_Pistachio.svg",
+    "secondaryLogoUrl": "https://erbaandco.com/wp-content/uploads/2023/03/RealBrokerLogo.svg",
     "secondaryLogoAlt": "",
     "organizerName": "Daniel Losier",
     "brokerageName": "Real Broker",
