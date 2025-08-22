88e3c3d4bce522ef85efe9df86f018b29b8d63ed
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-08-22 17:29:26 -0400

    Update Site

--- config.js
+++ config.js
@@ -35,7 +35,7 @@ const config = {
   "prize": {
     "images": [
       {
-        "src": "",
+        "src": "file:///Users/mateojackson/Downloads/replicate-prediction-ymesqq14ghrme0crteq8k8204r.webp",
         "alt": "Private chef cooking at a Toronto home"
       },
       {
@@ -168,7 +168,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1755897566436_0",
+      "id": "q_auto_1755898165642_0",
       "questionText": "What do you like the most about your current home?",
       "options": [
         {
@@ -190,7 +190,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1755897566437_1",
+      "id": "q_auto_1755898165642_1",
       "questionText": "Is a move on your radar in the near future?",
       "options": [
         {
@@ -212,7 +212,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1755897566437_2",
+      "id": "q_auto_1755898165642_2",
       "questionText": "What’s your current housing situation?",
       "options": [
         {
